86078b0f235ccb5d69f02f8cf20bea21e3b05dc2
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2025-04-16 02:12:16 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 // module.exports = defineConfig({
 //   transpileDependencies: true
 // });
+module.exports = {
+  outputDir: 'dist', // This specifies the 'dist' folder for the build output
+}
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../backend/restaurant_management'),
